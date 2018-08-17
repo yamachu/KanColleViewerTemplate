1e6c57032229d85da2a5fdda4d434cd7ae7bc0f4
yamachu <yamachu.dev@gmail.com>
2018-08-18 01:16:50 +0900

    Add Sample HTTP Request Handler

---
+++ src/js/gameWindowController.js
@@ -0,0 +1,13 @@
+(function () {
+    let responseHandler = require('./js/requestHandler.js');
+    window.game = window.game || {};
+    window.game.Window = document.getElementById('game_main');
+    responseHandler.init();
+
+    // Need WebContents, so call after dom-ready
+    window.game.Window.addEventListener('dom-ready', () => {
+        let content = window.game.Window.getWebContents();
+        window.game.Content = content;
+        responseHandler.initializeHandling();
+    }, { once: true });
+})();
