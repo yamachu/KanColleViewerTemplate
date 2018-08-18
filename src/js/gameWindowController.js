6f4cbd75df22b91c37861eaee07a4182b0ab8e4c
yamachu <yamachu.dev@gmail.com>
2018-08-18 15:00:58 +0900

    Apply Prettier

--- src/js/gameWindowController.js
+++ src/js/gameWindowController.js
@@ -1,13 +1,17 @@
-(function () {
-    let responseHandler = require('./js/requestHandler.js');
-    window.game = window.game || {};
-    window.game.Window = document.getElementById('game_main');
-    responseHandler.init();
+(function() {
+  let responseHandler = require("./js/requestHandler.js");
+  window.game = window.game || {};
+  window.game.Window = document.getElementById("game_main");
+  responseHandler.init();
 
-    // Need WebContents, so call after dom-ready
-    window.game.Window.addEventListener('dom-ready', () => {
-        let content = window.game.Window.getWebContents();
-        window.game.Content = content;
-        responseHandler.initializeHandling();
-    }, { once: true });
+  // Need WebContents, so call after dom-ready
+  window.game.Window.addEventListener(
+    "dom-ready",
+    () => {
+      let content = window.game.Window.getWebContents();
+      window.game.Content = content;
+      responseHandler.initializeHandling();
+    },
+    { once: true }
+  );
 })();
--- src/js/requestHandler.js
+++ src/js/requestHandler.js
@@ -1,49 +1,52 @@
-'use strict';
+"use strict";
 
-const { Subject } = require('rxjs');
+const { Subject } = require("rxjs");
 
 function initializeHandling() {
-    let content = window.game.Content;
+  let content = window.game.Content;
 
-    try {
-        content.debugger.attach('1.2');
-    } catch (err) {
-        console.error('Debugger attach failed : ', err);
-    };
+  try {
+    content.debugger.attach("1.2");
+  } catch (err) {
+    console.error("Debugger attach failed : ", err);
+  }
 
-    content.debugger.on('detach', (event, reason) => {
-        console.error('Debugger detached due to : ', reason);
-    });
+  content.debugger.on("detach", (event, reason) => {
+    console.error("Debugger detached due to : ", reason);
+  });
 
-    content.debugger.on('message', (event, method, params) => {
-        if (method === 'Network.responseReceived') {
-            if (params.response.mimeType === 'text/plain' && /^http:\/\/.*\/kcsapi\/.+$/.test(params.response.url)) {
-                content.debugger.sendCommand(
-                    'Network.getResponseBody',
-                    { 'requestId': params.requestId },
-                    (e, r) => {
-                        window.game.jsonResponse.next({
-                            URL: params.response.url,
-                            Body: r.body
-                        });
-                    }
-                );
-            }
-        } else if(method === 'Network.requestWillBeSent') {
-            // リクエスト欲しい時あったっけ…
-        }
-    });
+  content.debugger.on("message", (event, method, params) => {
+    if (method === "Network.responseReceived") {
+      if (
+        params.response.mimeType === "text/plain" &&
+        /^http:\/\/.*\/kcsapi\/.+$/.test(params.response.url)
+      ) {
+        content.debugger.sendCommand(
+          "Network.getResponseBody",
+          { requestId: params.requestId },
+          (e, r) => {
+            window.game.jsonResponse.next({
+              URL: params.response.url,
+              Body: r.body
+            });
+          }
+        );
+      }
+    } else if (method === "Network.requestWillBeSent") {
+      // リクエスト欲しい時あったっけ…
+    }
+  });
 
-    content.debugger.sendCommand('Network.enable');
+  content.debugger.sendCommand("Network.enable");
 
-    console.debug('Network request/response handling start');
+  console.debug("Network request/response handling start");
 }
 
 module.exports = {
-    // Call top-level
-    init: () => {
-        window.game.jsonResponse = window.game.jsonResponse || new Subject();
-    },
-    // Call after webview emitted dom-ready
-    initializeHandling: initializeHandling,
+  // Call top-level
+  init: () => {
+    window.game.jsonResponse = window.game.jsonResponse || new Subject();
+  },
+  // Call after webview emitted dom-ready
+  initializeHandling: initializeHandling
 };
